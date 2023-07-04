98461be6373ffd14af0aea7d61966b13acff7222
wancat <wancat@wancat.cc>
2023-07-05 04:51:46 +0800

    Add validate function

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 const memo = ref('')
 const templateId = ref('default')
+const errorMessage = ref('')
 
 const template = {
   accounts: [
@@ -11,22 +12,60 @@ const template = {
   ]
 }
 
-const accounts = ref([
+const accounts = reactive([
   { name: '', amount: null, commodity: '' },
   { name: '', amount: null, commodity: '' },
 ])
 
 const addAccount = () => {
-  accounts.value.push({ name: '', amount: null, commodity: '' })
+  accounts.push({ name: '', amount: null, commodity: '' })
 }
 
 const removeAccount = (index) => {
-  accounts.value.splice(index, 1)
+  accounts.splice(index, 1)
+}
+
+const validateInput = () => {
+  let nullCount = 0;
+  let totalAmount = 0;
+  const result = accounts.map((account, index) => {
+    const output = {}
+    output.name = account.name || template.accounts[index]?.name;
+    output.amount = account.amount || template.accounts[index]?.amount || null;
+    output.commodity = account.commodity || template.accounts[index]?.commodity;
+    if (output.amount === null) nullCount++;
+    else totalAmount += output.amount;
+    if (!output.name) {
+      throw `Account ${index + 1} cannot be empty.`
+    }
+    return output
+  });
+  console.log(result)
+
+  if (nullCount > 1) {
+    throw 'Only one account can have a empty amount.'
+  }
+  else if (nullCount === 0 && totalAmount !== 0) {
+    throw 'The total amount of every account should be 0.'
+  }
+  return result;
+}
+
+const onSubmit = () => {
+  try {
+    const result = validateInput()
+    console.log(result)
+    console.log('Submitted successfully')
+    errorMessage.value = ''
+  } catch (e) {
+    errorMessage.value = e
+    return
+  }
 }
 </script>
 
 <template>
-  <div class="container max-w-lg mx-auto">
+  <div class="container max-w-lg mx-auto px-3">
     <h1 class="text-3xl underline">Lazy 累記</h1>
     <p>Some dummy text</p>
     <div>
@@ -62,7 +101,7 @@ const removeAccount = (index) => {
         <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
           <button
             class="block bg-gray-100 hover:bg-white border px-3 py-2 rounded shadow disabled:shadow-none disabled:bg-gray-200 mt-5 hover:white"
-            :disabled="accounts.length <= 2" @click="removeAccount(index)">X</button>
+            v-if="accounts.length > 2" @click="removeAccount(index)">X</button>
         </div>
       </div>
       <button @click="addAccount"
@@ -74,7 +113,10 @@ const removeAccount = (index) => {
         <input v-model="memo" type="text" id="memo"
           class="shadow bg-gray-50 border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:shadow-outline" />
       </div>
-      <button
+      <div v-if="errorMessage" class="mt-4 text-red-600 border border-red-600 p-3 rounded">
+        {{ errorMessage }}
+      </div>
+      <button @click="onSubmit"
         class="block bg-gray-100 hover:bg-white border px-3 py-2 rounded shadow disabled:shadow-none disabled:bg-gray-200 mt-5 hover:white">Submit</button>
     </div>
   </div>
