c6d622b1db5e361c64db6cbf8ebae398f988e57a
wancat <wancat@wancat.cc>
2024-03-09 00:02:27 +0800

    feat: add testcase for register

--- ledger/exec_test.go
+++ ledger/exec_test.go
@@ -17,7 +17,7 @@ func TestGenCommandArgs(t *testing.T) {
 	assert.Equal(t, expectedArgs, args)
 }
 
-func TestExecuteLedgerCommand(t *testing.T) {
+func TestExecuteLedgerCommandBalance(t *testing.T) {
 	command := Command{
 		Command: "bal",
 		Input: strings.NewReader(`2024-01-01 restaurant
@@ -32,5 +32,22 @@ func TestExecuteLedgerCommand(t *testing.T) {
 "Assets:Checking","$-10"
 "Expenses:Food:Restaurant","$10"
 "total","0"
+`, out)
+}
+
+func TestExecuteLedgerCommandRegister(t *testing.T) {
+	command := Command{
+		Command: "reg",
+		Input: strings.NewReader(`2024-01-01 restaurant
+  Expenses:Food:Restaurant  $10
+  Assets:Checking
+`),
+	}
+	out, err := command.Execute()
+	t.Log(out)
+	assert.NoError(t, err)
+	assert.Equal(t, `"txnidx","date","code","description","account","amount","total"
+"1","2024-01-01","","restaurant","Expenses:Food:Restaurant","$10","$10"
+"1","2024-01-01","","restaurant","Assets:Checking","$-10","0"
 `, out)
 }
