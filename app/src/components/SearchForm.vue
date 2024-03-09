4358fdc2d1ef449be35cf9975d1d108de9ccce78
wancat <wancat@wancat.cc>
2024-03-09 12:58:49 +0800

    feat: add styling to search form

--- app/src/components/SearchForm.vue
+++ app/src/components/SearchForm.vue
@@ -28,24 +28,35 @@ const updateEndDate = (endDate?: string) => {
 </script>
 
 <template>
-  <div class="w-full">
-    <div>
-      <input
-        :value="modelValue.searchTerm"
-        @input="(event) => handleInput(updateSearchTerm)(event)"
-        type="text"
-        placeholder="Search term"
-      />
+  <div class="w-full md:w-2/3 px-3">
+    <input
+      :value="modelValue.searchTerm"
+      @input="(event) => handleInput(updateSearchTerm)(event)"
+      type="text"
+      placeholder="Search term"
+      class="w-full shadow border rounded p-2 mb-3"
+    />
+    <div class="w-full flex flex-row gap-5 justify-center mb-5">
+      <div class="">
+        <label for="beginDate" class="text-gray-500">From</label>
+        <input
+          id="beginDate"
+          :value="modelValue.beginDate"
+          @input="(event) => handleInput(updateBeginDate)(event)"
+          type="date"
+          class="w-full shadow border rounded p-2"
+        />
+      </div>
+      <div class="">
+        <label for="endDate" class="text-gray-500">To</label>
+        <input
+          id="endDate"
+          :value="modelValue.endDate"
+          @input="(event) => handleInput(updateEndDate)(event)"
+          type="date"
+          class="w-full shadow border rounded p-2"
+        />
+      </div>
     </div>
-    <input
-      :value="modelValue.beginDate"
-      @input="(event) => handleInput(updateBeginDate)(event)"
-      type="date"
-    />
-    <input
-      :value="modelValue.endDate"
-      @input="(event) => handleInput(updateEndDate)(event)"
-      type="date"
-    />
   </div>
 </template>
